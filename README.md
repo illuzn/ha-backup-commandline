4782786799e778f55c44c77831087e811a3674ef
Will <william.chau@illuzn.com>
2024-01-22 18:38:13 +1030

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ha-backup-crontab
-Crontab scripts for Home Assistant to deal with your backups
+# Home Assistant Backup Crontab
+Crontab scripts for Home Assistant to deal with your backups.
+
+Use this inconjunction with the Google Drive, Onedrive or your cloud backup solution of choice to implement your 3-2-1 backup strategy. 3 copies of backups. 2 on different storage media. 1 offsite backup.
+
+
---
+++ remote-backup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Customisations
+local_dir=/backup # Where to look for backups (if not default)
+remote_dir=/share/network # Where to copy backups to a secondary drive (where you have mounted your smb share)
+local_backups=50 # How many local backups to keep. N.B. This script cannot differentiate between full and partial backups. 0 is infinite and will skip this option.
+remote_backups=100 # How many remote backups to keep. N.B. This script cannot differentiate between full and partial backups. 0 is infinite and will skip this option.
+local_expire=30 # How many days until local backups are considered expired and deleted. 0 is infinite and will skip this option.
+remote_expire=90 # How many days until remote backups are considered expired and deleted. 0 is infinite and will skip this option.
+
+# Do not edit below here unless you know what you are doing.
+
+# Abort script on common failure modes.
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
+# Purge local by number
+if [! $local_backups -eq 0 ] ; then
+  n=0
+  while IFS= read -r -d '' -u 9
+  do
+    let ++n
+    if [ "$i" -gt "$local_backups" ]
+    then
+      rm -i "$REPLY"
+    fi
+  done 9< <(find [ $local_dir ] -type f -maxdepth 1 -print0 | sort -rz )
+fi
+# Purge local by days
+if [ ! $local_expire -eq 0 ] ; then
+  find [ $local_dir -type f -mtime -$local_expire ] -exec rm -i {} \;
+fi
+
+# Purge remote by number
+if [ ! $remote_backups -eq 0 ] ; then
+  n=0
+  while IFS= read -r -d '' -u 9
+  do
+    let ++n
+    if [ "$i" -gt "$remote_backups" ]
+    then
+      rm -i "$REPLY"
+    fi
+  done 9< <(find [ $remote_dir ] -type f -maxdepth 1 -print0 | sort -rz )
+fi
+
+# Purge remote by days
+if [ ! $remote_expire -eq 0 ] ; then
+  find [ $remote_dir -type f -mtime -$remote_expire ] -exec rm -i {} \;
+fi
+
+# Naive copy (no rsync) based upon file date only.
+cp -u $local_dir $remote_dir
